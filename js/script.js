9d73fd5deee4d78ae2d050b025824e3007b23139
esamia1 <esamia001@gmail.com>
2025-09-22 16:25:44 +0300

    Update transaction view
    Multiple pages are going to be utilized instead of single page for navigation

--- js/script.js
+++ js/script.js
@@ -8,46 +8,8 @@ const addBtn = document.getElementById("add-item");
 const gregWeekDays = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"]
 const gregMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul","Aug", "Sep", "Nov","Dec"];
 
-const addEntryForm = `
-    <div class="add-entry some-space">
-        <div>
-            <button><i class="fa-solid fa-arrow-left"></i></button>
-            <div>Expense</div>
-        </div>
-        <div>
-            <span>Income</span>
-            <span>Expense</span>
-            <span>Transfer</span>
-        </div>
-        <div>
-            <form action="">
-                <div>
-                    <label for="">Date</label>
-                    <input type="date" name="" id="">
-                    <input type="time" name="" id="">
-                </div>
-                <div>
-                    <label for="">Amount</label>
-                    <input type="number" name="" id="">
-                </div>
-                <div>
-                    <label for="">Category</label>
-                    <input type="text">
-                </div>
-                <div>
-                    <label for="">Account</label>
-                    <input type="text" name="" id="">
-                </div>
-                <div>
-                    <button>Done</button>
-                </div>
-            </form>
-        </div>
-    </div>`;
-
 //get date and time date format: Tue, Jul 26, 2025
 //time format: 17:28:21
-
 const getMyDate = new Date();
 
 //extract date
@@ -74,27 +36,3 @@ function updateTime() {
     //update display
     timeElement.textContent = `${myHours}:${myMinutes}:${mySeconds}`;
 }
-
-//function to populate html for transaction view
-const addTransactionView = () => {
-   //create top navigation
-   const backNavDiv = document.createElement("div");
-   
-   //create some content to add
-   const greeTing = document.createTextNode("Hi there and greetings!");
-
-   //add content to the created element
-   backNavDiv.appendChild(greeTing);
-
-   wrapperElement.appendChild(backNavDiv);
-};
-
-
-//Adding New Items Transcations Event listener
-addBtn.addEventListener("click", () => {
-    //clear the main container element
-    contentElement.innerHTML = addEntryForm;
-
-    //call the new function
-    addTransactionView();
-});
