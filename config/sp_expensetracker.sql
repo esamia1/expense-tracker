c35ce9741f0975bcc70f42dc510ad142247d7317
esamia1 <esamia001@gmail.com>
2025-10-03 11:08:12 +0300

    Create database for expense management

---
+++ config/sp_expensetracker.sql
@@ -0,0 +1,27 @@
+-- Host: 127.0.0.1
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sp_expensetracker`
+--
+CREATE DATABASE IF NOT EXISTS `sp_expensetracker` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sp_expensetracker`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table ``
+--
+
+
+
+
