fe1e3839f18081e4a3e3c20b8c50e8bd093213bd
esamia1 <esamia001@gmail.com>
2025-12-07 17:22:40 +0300

    Fixed undefined month display

--- js/script.js
+++ js/script.js
@@ -4,23 +4,12 @@ const timeElement = document.getElementById("time-text");
 const contentElement = document.getElementById("content");
 const addBtn = document.getElementById("add-item");
 
-//Constants in the App
-const gregWeekDays = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"]
-const gregMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul","Aug", "Sep", "Nov","Dec"];
-
 //get date and time date format: Tue, Jul 26, 2025
 //time format: 17:28:21
 const getMyDate = new Date();
 
-//extract date
-const myWeekDay = getMyDate.getDay();
-const myMonthDay = getMyDate.getDate();
-const myMonth = getMyDate.getMonth();
-const myYear = getMyDate.getFullYear();
-
-
 //display date and time
-dateElement.textContent = `${gregWeekDays[myWeekDay]}, ${gregMonths[myMonth]} ${myMonthDay}, ${myYear}`;
+dateElement.textContent = myDateFormat(getMyDate);
 setInterval(updateTime, 1000);
 
 //Update time function
@@ -28,11 +17,22 @@ function updateTime() {
     //get date and time
     const getMyDate = new Date();
 
+    /*
     //extract time
     const myHours = String(getMyDate.getHours()).padStart(2, "0");
     const myMinutes = String(getMyDate.getMinutes()).padStart(2, "0");
     const mySeconds = String(getMyDate.getSeconds()).padStart(2, "0");
+    */
 
     //update display
-    timeElement.textContent = `${myHours}:${myMinutes}:${mySeconds}`;
+    timeElement.textContent = getMyDate.toLocaleTimeString("en-GB");
+}
+
+//function to produce date format: Sun, Dec 7, 2025
+function myDateFormat(date) {
+    //Constants in the App
+    const gregWeekDays = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"];
+    const gregMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul","Aug", "Sep", "Oct", "Nov","Dec"];
+
+    return `${gregWeekDays[getMyDate.getDay()]}, ${gregMonths[getMyDate.getMonth()]} ${getMyDate.getDate()}, ${getMyDate.getFullYear()}`;
 }
